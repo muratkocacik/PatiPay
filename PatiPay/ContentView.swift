2f837b52cf9fc5236e3cdd049fe07ed145e4a44c
Murat Kocacık <muratkocacik06@gmail.com>
2025-03-17 20:51:19 +0300

    Initial Commit

---
+++ PatiPay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PatiPay
+//
+//  Created by Murat KOCACIK on 17.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PatiPay/PatiPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatiPayApp.swift
+//  PatiPay
+//
+//  Created by Murat KOCACIK on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct PatiPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
